cf828ba14e49fdf7d459735e989cf7a865e25d8a
Christopher <chrismbogo97@gmail.com>
2024-02-15 00:43:26 +0300

    first commit

---
+++ src/components/About/Techstack.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import { DiJavascript1, DiReact, DiNodejs, DiMongodb } from "react-icons/di";
+import { SiMui, SiFirebase, SiRubyonrails } from "react-icons/si";
+import { FaGit } from "react-icons/fa6";
+
+function Techstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJavascript1 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiRubyonrails />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiNodejs />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiReact />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiMongodb />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiFirebase />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiMui />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <FaGit />
+      </Col>
+    </Row>
+  );
+}
+
+export default Techstack;
